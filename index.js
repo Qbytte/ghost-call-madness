60e8e30e970e74e644efa2711be0a2227e14da45
Polter97 <hector.loyo@hotmail.com>
2021-05-03 12:57:18 -0600

    Added init() and script auto execution

--- index.js
+++ index.js
@@ -9,13 +9,18 @@ const setup = () => {
 
     if (
       confirm(
-        `Estos datos son correctos? \nCLI User ID: ${userID} \nStation Name: ${stationID}`
+        `¿Estos datos son correctos? \nCLI User ID: ${userID} \nStation Name: ${stationID}`
       ) &&
       userID &&
       stationID
     ) {
-      document.cookie = `ghostUserID=${userID}; expires=Sun, 31 Dec 2022 12:00:00 UTC; secure`;
-      document.cookie = `ghostStationID=${stationID}; expires=Sun, 31 Dec 2022 12:00:00 UTC; secure`;
+      const gd = new Date();
+      document.cookie = `ghostUserID=${userID}; expires=Sun, 31 Dec ${
+        gd.getFullYear() + 1
+      } 12:00:00 UTC; secure`;
+      document.cookie = `ghostStationID=${stationID}; expires=Sun, 31 Dec ${
+        gd.getFullYear() + 1
+      } 12:00:00 UTC; secure`;
       bool = true;
     } else {
       alert('Ingresa los datos nuevamente');
@@ -45,13 +50,13 @@ const submitData = (callID) => {
     boxDetails: 'r4cfd057baebc4cf8a7fa89e5d478a639',
   };
 
-  const d = new Date();
+  const gd = new Date();
 
   let data = {
     boxUserID: userID,
     boxStationID: stationID,
-    boxDate: `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`,
-    boxTime: `${d.getHours()}:${d.getMinutes()}`,
+    boxDate: `${gd.getMonth() + 1}/${gd.getDate()}/${gd.getFullYear()}`,
+    boxTime: `${gd.getHours()}:${gd.getMinutes()}`,
     boxCallID: callID,
     dropIncident: '',
     boxDetails:
@@ -62,8 +67,11 @@ const submitData = (callID) => {
     try {
       switch (q) {
         case 'boxDetails':
-          document.querySelector(`.office-form-textfield textarea`).value =
-            data[q];
+          const textarea = document.querySelector(
+            `.office-form-textfield textarea`
+          );
+          textarea.focus();
+          textarea.value = data[q];
           break;
         case 'dropIncident':
           document.querySelector(`#${form[q]}`).click();
@@ -75,10 +83,13 @@ const submitData = (callID) => {
           const input = document.querySelector(
             `[aria-labelledby="QuestionId_${form[q]}"] input`
           );
+          input.focus();
           input.value = data[q];
       }
     } catch (err) {
-      console.log(`Este sitio no cuenta con un formulario. ${err}`);
+      console.log(
+        `El formulario de este sitio no coincide con el preestablecido. \n${err}`
+      );
       return;
     }
   }
@@ -127,8 +138,13 @@ const init = () => {
     </div>
   `;
 
-  const header = document.querySelector('.office-form-title');
-  header.innerHTML += app;
+  try {
+    const header = document.querySelector('.office-form-title');
+    header.innerHTML += app;
+  } catch (err) {
+    console.log(`Este sitio no cuenta con un formulario. \n${err}`);
+    return;
+  }
 
   updateCredentials();
 
@@ -142,6 +158,10 @@ const init = () => {
 
     if (id.length === 10 && id.match(/[a-z]/gi) === null) {
       submitData(id);
+    } else {
+      alert(
+        'El call ID consta de 10 digitos y no puede contener un valor alfanumérico'
+      );
     }
   });
 };
