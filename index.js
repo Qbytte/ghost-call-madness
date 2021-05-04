9208ad43392dc4d63c8cfe2fbbfe15021da718af
Omar Loyo <omarloyo121@gmail.com>
2021-05-04 17:32:25 -0600

    Adapted js to new plan

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ const setup = () => {
       stationID
     ) {
       const gd = new Date();
-      document.cookie = `ghostUserID=${userID}; expires=Sun, 31 Dec ${
-        gd.getFullYear() + 1
-      } 12:00:00 UTC; secure`;
-      document.cookie = `ghostStationID=${stationID}; expires=Sun, 31 Dec ${
-        gd.getFullYear() + 1
-      } 12:00:00 UTC; secure`;
+      document.cookie = `ghostUserID=${userID}; expires=Sun, 31 Dec ${gd.getFullYear() + 1
+        } 12:00:00 UTC; secure`;
+      document.cookie = `ghostStationID=${stationID}; expires=Sun, 31 Dec ${gd.getFullYear() + 1
+        } 12:00:00 UTC; secure`;
       bool = true;
     } else {
-      alert('Ingresa los datos nuevamente');
+      if (!confirm('¿Quieres ingrasar los datos nuevamente?')) {
+        bool = true;
+      }
     }
   } while (!bool);
 };
@@ -34,67 +34,6 @@ const getCookie = (name) => {
   if (parts.length === 2) {
     return parts.pop().split(';').shift();
   }
-};
-
-const submitData = (callID) => {
-  const userID = getCookie('ghostUserID');
-  const stationID = getCookie('ghostStationID');
-
-  const form = {
-    boxUserID: 'r9cfb39c2e81a408bb43a35038f95fd5c',
-    boxStationID: 'rc7377d2c86864253941b9e1655c322f5',
-    boxDate: 'r3c66eec0605b4110abc3dd8382f6cee1',
-    boxTime: 'rddac66fdc58946cba49aa45e8a8982f1',
-    boxCallID: 'rdd14bbfd20ac4b698483d6894e9b29fe',
-    dropIncident: 'SelectId_0',
-    boxDetails: 'r4cfd057baebc4cf8a7fa89e5d478a639',
-  };
-
-  const gd = new Date();
-
-  let data = {
-    boxUserID: userID,
-    boxStationID: stationID,
-    boxDate: `${gd.getMonth() + 1}/${gd.getDate()}/${gd.getFullYear()}`,
-    boxTime: `${gd.getHours()}:${gd.getMinutes()}`,
-    boxCallID: callID,
-    dropIncident: '',
-    boxDetails:
-      'Nobody was on the line, gave ghost call script and released the line',
-  };
-
-  for (q in form) {
-    try {
-      switch (q) {
-        case 'boxDetails':
-          const textarea = document.querySelector(
-            `.office-form-textfield textarea`
-          );
-          textarea.value = data[q];
-          break;
-        case 'dropIncident':
-          document.querySelector(`#${form[q]}`).click();
-          setTimeout(() => {
-            document.querySelector(`[aria-label="Ghost Call"]`).click();
-          }, 150);
-          break;
-        default:
-          const input = document.querySelector(
-            `[aria-labelledby="QuestionId_${form[q]}"] input`
-          );
-          input.value = data[q];
-      }
-    } catch (err) {
-      console.log(
-        `El formulario de este sitio no coincide con el preestablecido. \n${err}`
-      );
-      return;
-    }
-  }
-  // document.querySelector('.__submit-button__').click(); // Don't uncomment this line while debugging
-
-  // window.location =
-  //   'https://forms.office.com/Pages/ResponsePage.aspx?id=5cDDXNoiuU2Zz34io0bvPEGhD5z2OjhLuRf4EFJZBGlUM1VCQUsySFJOSlgxQU80NkNXM1lBR1pIUS4u'; // Nor this one
 };
 
 const updateCredentials = () => {
@@ -108,48 +47,54 @@ const updateCredentials = () => {
   stationSpan.textContent = stationID;
 };
 
-const init = () => {
-  if (!getCookie('ghostUserID') && !getCookie('ghostStationID')) {
+const testData = () => {
+  const gd = new Date();
+
+  const link = `https://docs.google.com/forms/d/e/1FAIpQLSd470VihtYsAQsX1Z4KGYw5_kkdzneAgdjguK2_Z4V5_meaPg/viewform?usp=pp_url
+        &entry.1723038825=${userID}
+        &entry.1660251929=${stationID}
+        &entry.1236880167=${date}
+        &entry.757929743=${hr}
+        &entry.1241883286=${callId}
+        &entry.1266643092=Ghost+Call
+        &entry.1752359905=${desc}`
+}
+
+const submitData = (callId) => {
+  
+  const userID = getCookie('ghostUserID');
+  const stationID = getCookie('ghostStationID');
+  const gd = new Date();
+
+  let yr = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(gd);
+  let mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(gd);
+  let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(gd);
+  let hr = new Intl.DateTimeFormat('en', { hour: '2-digit', hour12: false}).format(gd);
+  let mn = new Intl.DateTimeFormat('en', { minute: '2-digit'}).format(gd);
+  
+  const date = `${yr}-${mo}-${da}`;
+  const hour = `${hr}:${mn}`;
+  const desc = 'Nobody was on the line.';
+
+  const link = `https://docs.google.com/forms/d/e/1FAIpQLSd470VihtYsAQsX1Z4KGYw5_kkdzneAgdjguK2_Z4V5_meaPg/viewform?usp=pp_url&entry.1723038825=${userID}&entry.1660251929=${stationID}&entry.1236880167=${date}&entry.757929743=${hour}&entry.1241883286=${callId}&entry.1266643092=Ghost+Call&entry.1752359905=${desc}`;
+
+  window.location = link;
+  setTimeout(() => {
+    document.querySelector('[jsname="M2UYVd"]').click();
+  }, 2000);
+}
+
+(() => {
+  if(!getCookie('ghostUserID') && !getCookie('ghostStationID')){
     setup();
   }
-
-  const app = `
-    <div class="container">
-      <h1>Ghost call madness</h1>
-      <div class="creds-showcase">
-        <strong>CLI user: </strong><span id="js-userID"></span>
-        <strong>Station ID: </strong><span id="js-stationID"></span>
-      </div>
-      <button class="button" id="creds-btn">Change credentials</button>
-      <div class="call-form">
-        <label for="call-id">Call ID: </label>
-        <input
-          class="input"
-          type="text"
-          name="call-id"
-          id="call-id"
-          maxlength="10"
-          autocomplete="off"
-        />
-        <button type="button" class="button" id="submit-btn">Submit ID</button>
-      </div>
-    </div>
-  `;
-
-  try {
-    const header = document.querySelector('.office-form-title');
-    header.innerHTML += app;
-  } catch (err) {
-    console.log(`Este sitio no cuenta con un formulario. \n${err}`);
-    return;
-  }
-
   updateCredentials();
 
   document.querySelector('#creds-btn').addEventListener('click', () => {
     setup();
     updateCredentials();
   });
+
   document.querySelector('#submit-btn').addEventListener('click', () => {
     let id = document.querySelector('#call-id').value;
     id = id.replace(' ', '');
@@ -162,8 +107,4 @@ const init = () => {
       );
     }
   });
-};
-
-(() => {
-  setTimeout(init, 1000);
 })();
