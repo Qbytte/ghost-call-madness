1f1d1dda734c3c32fd370d549577269b134f32ea
Omar Loyo <78561511+Qbytte@users.noreply.github.com>
2021-05-06 17:52:16 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -48,13 +48,18 @@ const submitData = (callId) => {
     hour: '2-digit',
     hour12: false,
   }).format(gd);
-  const mn = new Intl.DateTimeFormat('en', { minute: '2-digit' }).format(gd);
+  const mn = new Intl.DateTimeFormat('en', { minute: 'numeric' }).format(gd);
 
   const date = `${yr}-${mo}-${da}`;
-  const hour = `${hr}:${mn}`;
+  let hour;
+  if(mn <= 9){
+    hour = `${hr}:0${mn}`;  
+  }else{
+    hour = `${hr}:${mn}`;
+  }
   const desc = 'Nobody was on the line.';
 
-  const link = `https://docs.google.com/forms/d/e/1FAIpQLSd470VihtYsAQsX1Z4KGYw5_kkdzneAgdjguK2_Z4V5_meaPg/viewform?usp=pp_url&entry.1723038825=${userID}&entry.1660251929=${stationID}&entry.1236880167=${date}&entry.757929743=${hour}&entry.1241883286=${callId}&entry.1266643092=Ghost+Call&entry.1752359905=${desc}`;
+  const link = `https://docs.google.com/forms/d/e/1FAIpQLSd470VihtYsAQsX1Z4KGYw5_kkdzneAgdjguK2_Z4V5_meaPg/viewform?usp=pp_url&entry.1723038825=${userID}&entry.1660251929=${stationID}&entry.1236880167=${date}&entry.757929743=${hour}&entry.1241883286=${callId}&entry.1266643092=Ghost+call&entry.1752359905=${desc}`;
 
   const formWin = window.open(link, 'Autofill Form');
   setTimeout(() => {
